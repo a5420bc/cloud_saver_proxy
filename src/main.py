a260a7863a28a5959f07493151c32d918a29ce0a
bingjie6 <bingjie6@staff.weibo.com>
2025-07-16 19:11:03 +0800

    端口配置化

--- src/main.py
+++ src/main.py
@@ -151,4 +151,4 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host=config["server"]["host"], port=config["server"]["port"])
